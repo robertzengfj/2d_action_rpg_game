a41292dec46b59cf532537aed165fc6bd718bcf2
robertzengfj <zengjianze@msn.cn>
2025-05-12 09:35:22 +0800

    add enemy and palyer health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public void ChangeHealth(int amount){
+        currentHealth+= amount;
+        if(currentHealth<=0){
+            gameObject.SetActive(false);
+        }
+    }
+}
