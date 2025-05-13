4d6ef9e4603d778887ccd78f838fca995a1e0c04
robertzengfj <zengjianze@msn.cn>
2025-05-13 09:54:35 +0800

    add health text and health animator

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
 
+    public TMP_Text healthText;
+
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        
+        healthText.text = "HP: " + currentHealth.ToString()+" / "+maxHealth.ToString();
+    }
+
     public void ChangeHealth(int amount){
         currentHealth+= amount;
+        healthTextAnim.Play("TextUpdate");
+        healthText.text = "HP: " + currentHealth.ToString()+" / "+maxHealth.ToString();
         if(currentHealth<=0){
             gameObject.SetActive(false);
         }
