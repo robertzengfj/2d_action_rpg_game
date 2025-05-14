be8431ac21bbca20c410dda44850ff739449c3a2
robertzengfj <zengjianze@msn.cn>
2025-05-14 09:43:29 +0800

    Setting up enemy pursuit

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -7,6 +7,12 @@ public class Enemy_Combat : MonoBehaviour
     public int damage=1;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        // Check if the collided object has the tag "Player"
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Call the method to deal damage to the player
+           collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        }
+        
     }
 }
---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Transform player;
+
+    private bool isChasing;
+
+    public float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isChasing==true){
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity=direction*speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Player"){
+            Debug.Log("Player detected");
+            if(player==null){
+            player = collision.transform;
+            }
+            isChasing = true;
+        }
+        //isChasing = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Player"){
+            Debug.Log("Player out of range");
+            rb.velocity = Vector2.zero;
+         isChasing = false;
+        }
+    }
+}
