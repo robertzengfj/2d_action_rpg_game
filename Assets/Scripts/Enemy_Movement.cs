ec2de811dd75690f653632c8efce07ac8d2194ba
robertzengfj <zengjianze@msn.cn>
2025-05-15 10:06:47 +0800

    set enemy animation

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,51 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Enemy_Movement : MonoBehaviour
 {
+    private EnemyState enemyState;
     private Rigidbody2D rb;
     private Transform player;
 
-    private bool isChasing;
+    public Animator anim;
+
+    // private bool isChasing;
 
     public float speed = 4f;
 
-    private int faceingDirection = -1;   
+    private int faceingDirection = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        ChangeState(EnemyState.Idle);
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isChasing==true){
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.velocity=direction*speed;
+        if (enemyState == EnemyState.Chasing)
+        {
+            // Check if the player is to the left or right of the enemy
+            if (player != null)
+            {
+                if ((player.position.x > transform.position.x && faceingDirection == -1) || (player.position.x < transform.position.x && faceingDirection == 1))
+                {
+                    Flip();
+                }
+                Vector2 direction = (player.position - transform.position).normalized;
+                rb.velocity = direction * speed;
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag=="Player"){
+        if (collision.gameObject.tag == "Player")
+        {
             Debug.Log("Player detected");
-            if(player==null){
-            player = collision.transform;
+            if (player == null)
+            {
+                player = collision.transform;
             }
-            isChasing = true;
+            //isChasing = true;
+             ChangeState(EnemyState.Chasing);
         }
         //isChasing = true;
+       
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.tag=="Player"){
+        if (collision.gameObject.tag == "Player")
+        {
             Debug.Log("Player out of range");
             rb.velocity = Vector2.zero;
-         isChasing = false;
+           // isChasing = false;
+            ChangeState(EnemyState.Idle);
         }
+       
+    }
+    void Flip()
+    {
+        faceingDirection *= -1;
+        // Vector3 localScale = transform.localScale;
+        // localScale.x *= -1;
+        // transform.localScale = localScale;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+    void ChangeState(EnemyState newState)
+    {
+        //Exit the current state
+        if (enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", false);
+        }
+        else if (enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("IsChasing", false);
+        }
+        //Update our current state
+        enemyState = newState;
+        if (enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", true);
+        }
+        else if (enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("IsChasing", true);
+        }
+
+
     }
 }
+    public enum EnemyState
+    {
+        Idle,
+        Chasing,
+        Attack
+    }
